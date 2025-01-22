82c3d331f2479ae2100e36e3658e6808ae52bb98
shashwat-naik <naikshashwat3@gmail.com>
2025-01-22 23:44:07 +0530

    Transfer Page Section

---
+++ lib/pages/top_up.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TopUpPage extends StatefulWidget {
+  const TopUpPage({super.key});
+
+  @override
+  State<TopUpPage> createState() => _TopUpPageState();
+}
+
+class _TopUpPageState extends State<TopUpPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello"),
+    );
+  }
+}
--- lib/pages/transfer_money.dart
+++ lib/pages/transfer_money.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_pay/pages/top_up.dart';
 import 'package:smart_pay/widgets/credit_card.dart';
 
 class TransferMoney extends StatelessWidget {
@@ -15,26 +16,159 @@ class TransferMoney extends StatelessWidget {
         title: Text("Transfer"),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Choose cards",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Choose cards",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                SizedBox(height: 12,),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(),
-                )
-              ],
-            )
-          ],
+                  SizedBox(
+                    height: 12,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                        children: List.generate(
+                            3,
+                            (index) => Padding(
+                                  padding:
+                                      EdgeInsets.symmetric(horizontal: 5.0),
+                                  child: CreditCard(),
+                                ))),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Choose recipients",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 55,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Search contacts...',
+                          hintStyle: TextStyle(
+                            color: Colors.grey[400],
+                          ),
+                          border: InputBorder.none,
+                          prefixIcon: Icon(
+                            Icons.search,
+                            size: 30,
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(
+                          3,
+                          (index) => Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 130,
+                              height: 150,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(
+                                    color:
+                                        index == 0 ? Colors.teal : Colors.grey,
+                                  )),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  index == 0
+                                      ? const Padding(
+                                          padding: EdgeInsets.symmetric(
+                                              horizontal: 8.0),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              Icon(
+                                                Icons.check,
+                                                color: Colors.teal,
+                                              )
+                                            ],
+                                          ),
+                                        )
+                                      : const SizedBox(),
+                                  const SizedBox(height: 12),
+                                  const CircleAvatar(
+                                    radius: 20,
+                                    backgroundImage:
+                                        AssetImage("assets/person.jpeg"),
+                                  ),
+                                  const Text(
+                                    "Shashwat",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  const Text(
+                                    "Naik",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.bold),
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      )),
+                  SizedBox(height: 50),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const TopUpPage()));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        fixedSize: const Size(double.maxFinite, 60),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12))),
+                    child: const Text(
+                      "Continue",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
